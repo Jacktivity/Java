bd690d0cd7cf3a444cfafbc00ef449ca7350fe81
Jacktivity <jh140695@gmail.com>
2018-03-14 13:51:28 +0000

    Finished week 5

--- week05/src/arraySorter/SelectionSortTimer.java
+++ week05/src/arraySorter/SelectionSortTimer.java
@@ -6,7 +6,7 @@ public class SelectionSortTimer {
 	public static void main(String[] args) {
 		SelectionSort<Integer> sorter = new SelectionSort<Integer>();
 		RandomArray<Integer> generator = new RandomIntegerArray(1000000);
-		sorter.timeInMillis(generator, 10, 5);
+		sorter.timeInMillis(generator, 10, 1);
 	}
 
 }
